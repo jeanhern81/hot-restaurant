bf9a1212b39f0074c706441b8f9225be49118a06
jdlevish <45375460+jdlevish@users.noreply.github.com>
2020-07-18 11:47:17 -0700

    added routes fro some pages

--- server.js
+++ server.js
@@ -12,108 +12,44 @@ var PORT = 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Star Wars Characters (DATA)
+// Tables Array
 // =============================================================
 var Tables = [
     {
-        routeName: "yoda",
+
         name: "Yoda",
-        role: "Jedi Master",
-        age: 900,
-        forcePoints: 2000
+        phoneNumber: "7604445555",
+        email: "yoda@force.com",
+        uniqueId: 2000
     },
-    {
-        routeName: "darthmaul",
-        name: "Darth Maul",
-        role: "Sith Lord",
-        age: 200,
-        forcePoints: 1200
-    },
-    {
-        routeName: "obiwankenobi",
-        name: "Obi Wan Kenobi",
-        role: "Jedi Master",
-        age: 55,
-        forcePoints: 1350
-    }
+
 ];
-var Tables = [
-    {
-        routeName: "yoda",
-        name: "Yoda",
-        role: "Jedi Master",
-        age: 900,
-        forcePoints: 2000
-    },
-    {
-        routeName: "darthmaul",
-        name: "Darth Maul",
-        role: "Sith Lord",
-        age: 200,
-        forcePoints: 1200
-    },
-    {
-        routeName: "obiwankenobi",
-        name: "Obi Wan Kenobi",
-        role: "Jedi Master",
-        age: 55,
-        forcePoints: 1350
-    }
-];
-var Tables = [
-    {
-        routeName: "yoda",
-        name: "Yoda",
-        role: "Jedi Master",
-        age: 900,
-        forcePoints: 2000
-    },
-    {
-        routeName: "darthmaul",
-        name: "Darth Maul",
-        role: "Sith Lord",
-        age: 200,
-        forcePoints: 1200
-    },
-    {
-        routeName: "obiwankenobi",
-        name: "Obi Wan Kenobi",
-        role: "Jedi Master",
-        age: 55,
-        forcePoints: 1350
-    }
-];
+
+
 
 // Routes
 // =============================================================
 
 // Basic route that sends the user first to the AJAX Page
 app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "view.html"));
+    res.sendFile(path.join(__dirname, "home.html"));
 });
 
-app.get("/add", function (req, res) {
-    res.sendFile(path.join(__dirname, "add.html"));
+app.get("/reserve", function (req, res) {
+    res.sendFile(path.join(__dirname, "reserve.html"));
 });
 
 // Displays all characters
-app.get("/api/characters", function (req, res) {
-    return res.json(characters);
+app.get("/api/tables", function (req, res) {
+    return res.json(Tables);
 });
 
 // Displays a single character, or returns false
-app.get("/api/characters/:character", function (req, res) {
-    var chosen = req.params.character;
+app.get("/api/tables/", function (req, res) {
 
-    console.log(chosen);
 
-    for (var i = 0; i < characters.length; i++) {
-        if (chosen === characters[i].routeName) {
-            return res.json(characters[i]);
-        }
-    }
 
-    return res.json(false);
+    return res.json(Tables);
 });
 
 // Create New Characters - takes in JSON input
